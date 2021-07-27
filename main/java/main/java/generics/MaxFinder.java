40e4be66dc54633b7cee35caf4ec6dd55d637955
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-27 10:40:00 +0530

    refactoring code

--- main/java/main/java/generics/MaxFinder.java
+++ main/java/main/java/generics/MaxFinder.java
@@ -2,44 +2,24 @@ package main.java.generics;
 
 import java.util.Arrays;
 
-public class MaxFinder<T extends Comparable<T>>  {
-    T[] elements;
-
-    public  MaxFinder(T[] elements) {
-        this.elements = elements;
-    }
-
-    public T maximum() {
-        return MaxFinder.findMax(elements);
-    }
-
-    public static <T> void printMax(T[] elements, T max) {
-        for (T element : elements) {
-            System.out.print(element);
+public class MaxFinder  {
+    public static <T extends Comparable<T>> T findMax(T[] elements){
+        T max = elements[0];
+        for (T i: elements){
+            if(i.compareTo(max) > 0)
+                max = i;
         }
-        System.out.printf("Max is %s \n", max);
-    }
-
-    public static <T extends Comparable<T>> T findMax(T[] elements) {
-        Arrays.sort(elements);
-        int length = elements.length;
-        T max = elements[length - 1];
-
         return max;
     }
 
-
-
-
-
     public static void main(String[] args) {
         Integer[] integerValue = {10, 34, 27};
-        Float[] floatValue = {16.75f, 2.3f, 8.76f, 45.76f};
-        String[] stringValue = {"apple", "banana", "orange", "peach", "pomegranate"};
+        Float[]  floatValue = {16.75f, 2.3f, 8.76f};
+        String[] stringValue = {"apple", "banana", "orange"};
 
-        new MaxFinder(integerValue).maximum();
-        new MaxFinder(floatValue).maximum();
-        new MaxFinder(stringValue).maximum();
+        findMax(integerValue);
+        findMax(floatValue);
+        findMax(stringValue);
 
 
     }
